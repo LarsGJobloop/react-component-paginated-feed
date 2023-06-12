828afb0023f376b730f5479f5dd3ab514f6901bb
LarsG <lars.gunnar@jobloop.no>
2023-06-12 18:41:04 +0000

    move responsibility for overflow handling of articles to caller

--- src/data/articles/articlesHandlers.js
+++ src/data/articles/articlesHandlers.js
@@ -2,27 +2,15 @@ import response from "./articlesData.json";
 
 /**
  * Get a slice from the articles array
- * @param {*} pageNumber 
- * @returns 
+ * @param {*} pageNumber
+ * @returns
  */
 export function getFeedPage(pageNumber) {
-  // Handle negative pageNumber
-  if (pageNumber < 0) {
-    pageNumber = 0
-  }
-  
+  // Sets the amount of articles returned
   const sliceSize = 10;
-
-  // Handle overflowing pageNumber
-  if (pageNumber > (response.articles.length / sliceSize)) {
-    pageNumber = (response.articles.length / sliceSize)
-  }
-
 
   const start = pageNumber * sliceSize;
   const end = start + sliceSize;
-
-  console.log(`Fetching article set: ${start} - ${end}`)
 
   return response.articles.slice(start, end);
 }
