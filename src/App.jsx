03d94521aad41d409345d15482ba90193b5a9ff5
LarsG <lars.gunnar@jobloop.no>
2023-06-12 21:49:53 +0000

    add comments about spread performance impact

--- src/App.jsx
+++ src/App.jsx
@@ -17,25 +17,35 @@ import {
   getArticleCount,
 } from "./data/articles/articlesHandlers";
 
-/**
- * You'll likely want to take any generic component
- * and create a specific implementation
- * tailored for this project.
- *
- * Done here by creating a thin wrapper around
- * the component and passing in the values
- * we want, limiting the options for the end user
- * (saving their mental capacity).
- */
+// You'll likely want to take any generic component
+// and create a specific implementation
+// tailored for this project.
+// Done here by creating a thin wrapper around
+// the component, passing in the values
+// we want and forwarding the rest.
+// Limiting the options for the end user
+// (saving some of their mental capacity).
 
 /**
  * A feed for articles
  */
 function ArticleFeed(props) {
+  // Here we are using the destructuring syntaxt
+  // to create a new object without the
+  // named keys. This is done immutably, ie without
+  // changing the orignial object.
+
+  // Performance Caveat!
+  // Creating new objects, copying, ++, is expensive
+  // which is what the spread operator does.
+  // Be mindful of this if you do something
+  // 100-1000++ of times per second.
   const {
     feedCard,
     getFeedLength,
     getFeedPage,
+    // Any remaining fields gets copied into
+    // this object. You can change the name if you want.
     ...rest
   } = props;
 
