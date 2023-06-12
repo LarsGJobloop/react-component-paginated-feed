dc8d839e135e7add2f0831c7b545fb27b0092f73
LarsG <lars.gunnar@jobloop.no>
2023-06-12 22:25:27 +0000

    move article feed definition to own folder

--- src/App.jsx
+++ src/App.jsx
@@ -6,58 +6,11 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Banner } from "./components/Banner/Banner";
 import { Footer } from "./components/Footer/Footer";
 
-// Dependencies of our basic Feed
+// Our basic feed
 import { Feed } from "./components/Feed/Feed";
 
-// Dependencies of Feed alternative B
-import { FeedAltB } from "./components/FeedAltB/FeedAltB";
-import { ArticleCard } from "./components/ArticleCard/ArticleCard";
-import {
-  getArticleSlice,
-  getArticleCount,
-} from "./data/articles/articlesHandlers";
-
-// You'll likely want to take any generic component
-// and create a specific implementation
-// tailored for this project.
-// Done here by creating a thin wrapper around
-// the component, passing in the values
-// we want and forwarding the rest.
-// Limiting the options for the end user
-// (saving some of their mental capacity).
-
-/**
- * A feed for articles
- */
-function ArticleFeed(props) {
-  // Here we are using the destructuring syntaxt
-  // to create a new object without the
-  // named keys. This is done immutably, ie without
-  // changing the orignial object.
-
-  // Performance Caveat!
-  // Creating new objects, copying, ++, is expensive
-  // which is what the spread operator does.
-  // Be mindful of this if you do something
-  // 100-1000++ of times per second.
-  const {
-    feedCard,
-    getFeedLength,
-    getFeedPage,
-    // Any remaining fields gets copied into
-    // this object. You can change the name if you want.
-    ...rest
-  } = props;
-
-  return (
-    <FeedAltB
-      feedCard={ArticleCard}
-      getFeedLength={getArticleCount}
-      getFeedPage={getArticleSlice}
-      {...rest}
-    />
-  );
-}
+// Our Article (tailored generic) feed
+import { ArticleFeed } from "./components/ArticleFeed/ArticleFeed";
 
 /**
  * The main application component
---
+++ src/components/ArticleFeed/ArticleFeed.jsx
@@ -0,0 +1,52 @@
+import style from "./style.module.css";
+
+import { FeedAltB } from "../FeedAltB/FeedAltB";
+import { ArticleCard } from "../ArticleCard/ArticleCard";
+import {
+  getArticleSlice,
+  getArticleCount,
+} from "../../data/articles/articlesHandlers";
+
+// You'll likely want to take any generic component
+// and create a specific implementation
+// tailored for this project.
+// Done here by creating a thin wrapper around
+// the component, passing in the values
+// we want and forwarding the rest.
+// Limiting the options for the end user
+// (saving some of their mental capacity).
+
+/**
+ * A feed for articles
+ */
+export function ArticleFeed(props) {
+  // Here we are using the destructuring syntaxt
+  // to create a new object without the
+  // named keys. This is done immutably, ie without
+  // changing the orignial object.
+
+  // Performance Caveat!
+  // Creating new objects, copying, ++, is expensive
+  // which is what the spread operator does.
+  // Be mindful of this if you do something
+  // 100-1000++ of times per second.
+  const {
+    className,
+    feedCard,
+    getFeedLength,
+    getFeedPage,
+    // Any remaining fields gets copied into
+    // this object. You can change the name if you want.
+    ...rest
+  } = props;
+
+  return (
+    <FeedAltB
+      className={className + " " + style["article-feed"]}
+      feedCard={ArticleCard}
+      getFeedLength={getArticleCount}
+      getFeedPage={getArticleSlice}
+      {...rest}
+    />
+  );
+}
